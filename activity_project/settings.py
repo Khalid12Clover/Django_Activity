00837d5eb200c5da688eb70147f225304314d936
Khalid12Clover <92978759+Khalid12Clover@users.noreply.github.com>
2025-11-25 12:45:14 +0530

    Update settings.py

--- activity_project/settings.py
+++ activity_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'activity_db',
         'USER': 'root',
         'PASSWORD': 'clover@123',
-        'HOST': 'localhost',
+        'HOST': '192.168.9.65',
         'PORT': '3306',
     }
 }
